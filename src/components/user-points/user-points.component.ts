68ee445a3a399410247f686776b0cc99b3da547b
Oscar Diaz <odiaz.dw@gmail.com>
2018-06-01 18:14:56 -0600

    points odometer.

--- src/components/user-points/user-points.component.ts
+++ src/components/user-points/user-points.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { startWith, delay, tap } from 'rxjs/operators';
+import { startWith, delay, tap, takeUntil, timeInterval, take } from 'rxjs/operators';
 import { UserProfileService } from './user-profile.service';
 import { UserPointsActions } from './user-points.actions';
 import { select } from '@angular-redux/store';
+import { interval, timer } from 'rxjs';
 
 @Component({
     selector: 'app-user-points',
@@ -12,15 +13,28 @@ import { select } from '@angular-redux/store';
 })
 export class UserPointsComponent implements OnInit {
     @select(['userPoints', 'points'])
-    points: number
+    points: Observable<number>
+    totalPoints: number = 0
     constructor() {
         Observable.of()
             .pipe(
                 startWith(null),
                 delay(0),
                 tap(() => UserPointsActions.get())
-            ).subscribe()
+            ).subscribe(result => {
+               this.points.subscribe(points => {
+                this.countPoints(points).subscribe()
+               })
+            })
     }
 
     ngOnInit(): void { }
+
+    countPoints (points) {
+        return interval(5).pipe(
+                startWith(null),
+                tap(x => this.totalPoints = this.totalPoints + 1),
+                take(points)
+            )
+    }
 }
