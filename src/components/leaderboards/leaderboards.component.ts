2fce6d9014926bb0db9d0c202fd1092e9419fe6b
Oscar Diaz <odiaz.dw@gmail.com>
2018-06-06 19:52:41 -0600

    get leaderboard all.

--- src/components/leaderboards/leaderboards.component.ts
+++ src/components/leaderboards/leaderboards.component.ts
@@ -1,12 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit } from '@angular/core';
+import { HttpWrapper } from '../../services/http-wrapper.service';
+import { config } from '../../config';
+import configFile from '../../config.file';
+import { Observable } from "rxjs/Observable";
+import { startWith, delay, tap } from "rxjs/operators";
+
+const {
+    protocol,
+    urlConfig: {
+        auth: { baseUrl, version, loginEndpoint },
+        leaderboards: { getAllLeaderboards }
+    }
+} = config(configFile);
 
 @Component({
     selector: 'app-leaderboards',
     templateUrl: './leaderboards.component.html',
     styleUrls: ['./leaderboards.component.scss']
 })
-export class LeaderboardsComponent implements OnInit {
-    constructor() { }
+export class LeaderboardsComponent implements AfterContentInit {
+    constructor(
+        private http: HttpWrapper<any>
+    ) { }
 
-    ngOnInit(): void { }
+    ngAfterContentInit() {
+        Observable.of()
+            .pipe(
+                startWith(null),
+                delay(0),
+                tap(() => {
+
+                })
+            )
+            .subscribe(() => {
+                this.getAll()
+                    .subscribe((result) => {
+                        console.log(result)
+                    })
+            });
+    }
+
+    getAll() {
+        const leaderboardsUrl = `${protocol}://${baseUrl}/${version}/${getAllLeaderboards}`;
+        return this.http.get(leaderboardsUrl)
+    }
 }
